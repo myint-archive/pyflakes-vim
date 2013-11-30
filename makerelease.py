f03d56fbcbb06b6c02a6e61f53c4587c296067db
Steven Myint <git@stevenmyint.com>
2013-11-30 15:39:14 -0800

    Support Python 3

--- makerelease.py
+++ makerelease.py
@@ -1,20 +1,27 @@
-'''
-creates the pyflakes-vim.zip bundle
-'''
+#!/usr/bin/env python
+
+"""Creates the pyflakes-vim.zip bundle."""
+
+from __future__ import absolute_import
+from __future__ import print_function
 
 import os.path
 import zipfile
 
+
 BUNDLE_FILENAME = 'pyflakes-vim.zip'
+
 
 def get_directory():
     return os.path.join(
         os.path.abspath(os.path.dirname(__file__)))
 
+
 def include_dir(d):
     return not any((d.startswith('.git'),
                     d.startswith('_trial_temp'),
                     d.startswith('.svn')))
+
 
 def include_file(f):
     return not any((f.endswith('.pyc'),
@@ -23,6 +30,7 @@ def include_file(f):
                     f == __file__,
                     f == '.DS_Store',
                     f.endswith('.diff')))
+
 
 def make_dist():
     z = zipfile.ZipFile(BUNDLE_FILENAME, 'w', zipfile.ZIP_DEFLATED)
@@ -35,13 +43,15 @@ def make_dist():
             name = os.path.join(root, f)
             if include_file(f):
                 zipname = os.path.relpath(name, base)
-                print zipname
+                print(zipname)
                 count += 1
                 z.writestr(zipname, open(name, 'rb').read())
     z.close()
 
-    print
-    print '%s is %d files, %d bytes' % (BUNDLE_FILENAME, count, os.path.getsize(BUNDLE_FILENAME))
+    print()
+    print('%s is %d files, %d bytes' %
+          (BUNDLE_FILENAME, count, os.path.getsize(BUNDLE_FILENAME)))
+
 
 if __name__ == '__main__':
     make_dist()
